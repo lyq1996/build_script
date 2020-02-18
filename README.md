0c0f6786ff517ee029deab0fb21c5d01670acbaf
lyq1996 <lyq123233@gmail.com>
2020-02-18 16:29:14 +0800

    update build.sh

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # build_script
 build script for OrangeFox TWRP, use for oneplus 5T
+
+# daemon
+
+```
+cd build_script
+nohup ./build.sh &
+```
--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#  ./build.sh 1 to clean build before make
+#  ./build.sh 1 to make clean before make
 work_path=$(dirname $(readlink -f $0))
 code_path=/home/ubuntu/rec
+tree_path=/home/ubuntu/android_device_oneplus_dumpling
 
 if [ -n "$1" ]; then
     clean_flag=$1
@@ -18,9 +19,17 @@ export BUILD_TYPE="Beta"
 # source directory
 cd $code_path
 
+# prepare tree
+if [ -d "device/oneplus/dumpling" ]; then
+    rm -rf device/oneplus
+fi
+echo "copy $tree_path to device/oneplus"
+mkdir -p device/oneplus
+cp -r $tree_path device/oneplus/dumpling
+
 # prepare modifed AromaFM.zip modify
 # backup origin AromaFM.zip
-if [ ! -f "vendor/recovery/FoxFiles/AromaFM/AromaFM-b.zip" ]; then  # last time build failed.
+if [ ! -f "vendor/recovery/FoxFiles/AromaFM/AromaFM-b.zip" ]; then  # if build canceled.
     echo "backup and replace AromaFM.zip to support Chinese"
     cp vendor/recovery/FoxFiles/AromaFM/AromaFM.zip vendor/recovery/FoxFiles/AromaFM/AromaFM-b.zip
     cp $work_path/AromaFM.zip vendor/recovery/FoxFiles/AromaFM/AromaFM.zip
@@ -29,7 +38,7 @@ fi
 source build/envsetup.sh
 lunch omni_dumpling-eng
 if [ "$clean_flag" = "1" ]; then
-    echo "clean build dir..."
+    echo "make clean before make"
     make clean
 fi
 make recoveryimage
@@ -37,6 +46,9 @@ make recoveryimage
 # restore origin AromaFM.zip
 echo "restore origin AromaFM.zip"
 mv vendor/recovery/FoxFiles/AromaFM/AromaFM-b.zip vendor/recovery/FoxFiles/AromaFM/AromaFM.zip
+
+# remove tree
+rm -rf device/oneplus
 
 if [ -f "$work_path/nohup.out" ]; then  # copy log
     cp $work_path/nohup.out $work_path/$(date "+%Y-%m-%d-%H-%M-%S").log
